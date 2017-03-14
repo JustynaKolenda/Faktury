f26eb72f66b133e67d7e05ca3659383c6e7e8983
Xirilla <j.trybalaa@gmail.com>
2017-03-14 13:20:09 +0100

    facture next step

--- app/Http/Controllers/FacturesController.php
+++ app/Http/Controllers/FacturesController.php
@@ -2,9 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Facture;
+use App\Http\Requests\FacturesRequest;
 use Illuminate\Http\Request;
 
 class FacturesController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dane = Facture::orderBy('id','ASC')->paginate(10);
+        return view('facture.index', compact('dane'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('facture.create', compact('dane'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Facture::create($request->all());
+        return redirect()->route('facture.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $facture = Facture::find($id);
+        return view('facture.edit', compact('facture'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(FacturesRequest $request, $id)
+    {
+        Facture::find($id)->update($request->all());
+        return redirect()->route('facture.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Facture::find($id)->delete();
+        return redirect()->route('facture.index');
+    }
 }
